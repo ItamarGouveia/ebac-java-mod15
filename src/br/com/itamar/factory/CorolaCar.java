dd9f7df44076b970918a90a0d1dc3642bcaf1845
ItamarGouveia <contato.itamargouveia@gmail.com>
2022-08-09 07:04:55 -0300

    mod15

---
+++ src/br/com/itamar/factory/CorolaCar.java
@@ -0,0 +1,9 @@
+package br.com.itamar.factory;
+
+
+public class CorolaCar extends Car{
+
+    public CorolaCar(int horsePower, String fuelSource, String color) {
+        super(horsePower, fuelSource, color);
+    }
+}
